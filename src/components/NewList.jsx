1d13b96d3c714c4b1d6abfbe789086433ad7ba33
daisy <gid5214@naver.com>
2023-01-30 01:33:46 +0900

    feat: list.css

--- src/components/NewList.jsx
+++ src/components/NewList.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import './list.css';
+import '../css/list.css';
 
 // import required modules
 import { Pagination } from 'swiper';
